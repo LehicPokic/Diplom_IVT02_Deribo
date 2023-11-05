1492aced448818f64d166e9ca1c0bd1389122399
OleksiyDer <leha.aderibo@yandex.ru>
2023-11-06 02:31:09 +0700

    save

--- desktop_app/mainwindow.cpp
+++ desktop_app/mainwindow.cpp
@@ -10,6 +10,11 @@ MainWindow::MainWindow(QWidget *parent)
     ui->radioButton_setting_climat->setChecked(true);
     ui->setting_operating_mode_w->setCurrentIndex(0);
     ui->settings_profile_w->setCurrentIndex(1);
+
+
+    statusBar()->clearMessage();
+    statusBar()->showMessage("Режим управления пока что не выбран");
+
 }
 
 MainWindow::~MainWindow()
@@ -46,5 +51,25 @@ void MainWindow::on_radioButton_setting_param_clicked()
 void MainWindow::on_radioButton_setting_climat_clicked()
 {
     ui->settings_profile_w->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    if (ui->radioButton_manual->isChecked())
+    {
+        statusBar()->clearMessage();
+        statusBar()->showMessage("Ручной режим управления");
+    }
+    else if (ui->radioButton_param->isChecked())
+    {
+        statusBar()->clearMessage();
+        statusBar()->showMessage("Режим поддержания параметров климата");
+    }
+    else
+    {
+        statusBar()->clearMessage();
+        statusBar()->showMessage("Режим управления профилями климата");
+    }
 }
 
--- desktop_app/mainwindow.h
+++ desktop_app/mainwindow.h
@@ -27,6 +27,8 @@ private slots:
 
     void on_radioButton_setting_climat_clicked();
 
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
